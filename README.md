f45ec96b775f9c4e3e21a5f2b7591613f89e2b87
kimserver <scd1212@naver.com>
2019-04-09 19:43:38 +0900

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+## 리액트 테스트인것이여
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers =[{
+  'id' : 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '김서버',
+  'age' : '25',
+  'sex' : '남자',
+  'job' : '학생'
+},{
+  'id' : 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '킹갓버',
+  'age' : '21',
+  'sex' : '여자',
+  'job' :  '프로게이머'
+},{
+  'id' : 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '그녀그뇬그녀',
+  'age' : '30',
+  'sex' :  '여자',
+  'job' : '사무직'
+}
+]
 
 class App extends Component {
+  
+ 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="gray-background">
+        {
+          customers.map(c => {
+            return (
+            <Customer
+              key = {c.id}
+              id = {c.id}
+              image ={c.image}
+              name ={c.name}
+              sex = {c.sex}
+              age = {c.age}
+              job = {c.job}
+              />   
+            )
+          })
+        }
+        
       </div>
     );
   }
